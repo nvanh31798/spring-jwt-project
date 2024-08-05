b05e9ea8a9e2cf0b33eba6cfaf58af8e9033faea
Ngo Anh <nvanh31798>
2024-08-05 14:39:04 +0700

    init project

---
+++ src/main/java/com/learning/spring_jwt_project/SpringJwtProjectApplication.java
@@ -0,0 +1,13 @@
+package com.learning.spring_jwt_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJwtProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/spring_jwt_project/SpringJwtProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.spring_jwt_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
